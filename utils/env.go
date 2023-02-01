30296a490ed1ba44cb80e0a9624ba07af2e1792e
Sniperq2 <ogldelphi@mail.ru>
2023-02-01 21:27:17 +0300

    Add configuration usin environment variables

---
+++ utils/env.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"log"
+	"os"
+)
+
+func SetConfig() string {
+	cacheLength, ok := os.LookupEnv("WEB_IMAGE_CROPPER_CACHE")
+	if !ok || len(cacheLength) == 0 {
+		// sets default value to 10
+		err := os.Setenv("WEB_IMAGE_CROPPER_CACHE", "10")
+		if err != nil {
+			log.Println("No image cropper cache set. Set default to 10")
+		}
+		return "10"
+	}
+
+	return cacheLength
+}
