de9ffc02bade69ff471b068910834173c5065801
Sniperq2 <ogldelphi@mail.ru>
2023-02-19 20:04:40 +0300

    Create const for env variable

--- utils/env.go
+++ utils/env.go
@@ -5,16 +5,18 @@ import (
 	"os"
 )
 
+const webImageCropperCache = "WEB_IMAGE_CROPPER_CACHE"
+
 type InitConfig struct {
 	CacheHandle Cache
 	CachePath   string
 }
 
 func SetConfig() string {
-	cacheLength, ok := os.LookupEnv("WEB_IMAGE_CROPPER_CACHE")
+	cacheLength, ok := os.LookupEnv(webImageCropperCache)
 	if !ok || len(cacheLength) == 0 {
 		// sets default value to 10
-		err := os.Setenv("WEB_IMAGE_CROPPER_CACHE", "10")
+		err := os.Setenv(webImageCropperCache, "10")
 		if err != nil {
 			log.Println("No image cropper cache set. Set default to 10")
 		}
